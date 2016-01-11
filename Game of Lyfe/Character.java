738f0708b086b02878b9b13b2e9a91c808e95423
Kevin <jh2933@gmail.com>
2016-01-11 16:43:47 +0000

    basic file structure added

---
+++ Game of Lyfe/Character.java
@@ -0,0 +1,26 @@
+/* Basic skeleton class for characters to implement later */
+
+import java.util.ArrayList;
+
+public abstract class Character {
+
+    private String name;
+    private int strength;
+    private int intelligence;
+    private int dexterity;
+    private ArrayList<Equip> items;
+    
+    public Character(String name) {
+        this = name;
+    }
+    
+    public abstract move() {
+        
+    }
+    
+    public abstract showStats() {
+        
+    }
+    
+    public abstract 
+}
