f438d5477e71aad4600c10deaab9bf305595019a
eccentricayman <aahmed3@stuy.edu>
2016-01-12 11:34:57 -0500

    added keys, stars, and coins to character class with methods

--- Character.java
+++ Character.java
@@ -4,24 +4,56 @@ import java.util.ArrayList;
 
 public abstract class Character {
     
+    //character stats
     private String name;
     private int strength;
     private int intelligence;
     private int dexterity;
+    private int luck;
+
+    //character items
     private ArrayList<Equip> items;
+    private int coins;
+    private int keys;
+    private int stars;
+
+    //character positions
     private int[] pos = new int[2];
     
     public Character(String name) {
         this.name = name;
     }
     
-    public abstract move() {
-        
+    public abstract void move();
+    
+    public abstract void showStats();
+    
+    public abstract void equip();
+
+    public void addCoins(int toadd) {
+	if (coins + toadd > 0) {
+	    coins += toadd;
+	}
+	else {
+	    coins = 0;
+	}
     }
     
-    public abstract showStats() {
-        
+    public void addKeys(int toadd) {
+	if (keys + toadd > 0) {
+	    keys += toadd;
+	}
+	else {
+	    keys = 0;
+	}
     }
     
-    public abstract
+    public void addStars(int toadd) {
+	if (stars + toadd > 0) {
+	    stars += toadd;
+	}
+	else {
+	    stars = 0;
+	}
+    }
 }
