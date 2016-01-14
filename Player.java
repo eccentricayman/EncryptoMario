89d51fdfd108e0bb9b24e2e0484eced169c1537f
Kevin <jh2933@gmail.com>
2016-01-14 16:45:47 +0000

    started player.java

---
+++ Player.java
@@ -0,0 +1,58 @@
+public Player extends Character {
+    
+    public Player(String name) {
+        super(name, 5, 5, 5, 5);
+    }
+    
+    public void move() {
+        for ( ; roll > 0 ; roll--) {
+            if (abs(x) + abs(y) == 5) {
+                if (x == 0) {
+                    if (y > 0) {
+                        x-=1;
+                        y-=1; }
+                    else {
+                        System.out.println("Would you like to move Up(0,-4) or Right(1,-4)?"); }
+                else if (y == 0) {
+                        if (x > 0) {
+                            System.out.println("Would you like to move Left(4,0) or Up(4,1)?"); }    
+                    else {
+                        x+=1;
+                        y-=1; } } }
+            else {
+                if (x == 0) y++;
+                else if (y == 0) x++;
+                else if (y > 0) {
+                    if (x > 0) {
+                        x--;
+                        y++; }
+                    else {
+                        x--;
+                        y--;
+                    }
+                }
+                else {
+                    if (x > 0) {
+                        x++;
+                        y++;
+                    }
+                    else {
+                        x--;
+                        y--;
+                    }
+                }
+            }
+            }
+        }
+    }
+    
+    public void showStats() {
+        
+    }
+    
+    public void equip() {
+        
+    }
+    
+    
+}
