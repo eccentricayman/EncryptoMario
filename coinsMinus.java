e00fa5d0ff8eecea857b621ac1a420a12a475679
Kevin <jh2933@gmail.com>
2016-01-24 00:16:48 +0000

    Dimbling, coins+=

--- coinsMinus.java
+++ coinsMinus.java
@@ -5,7 +5,7 @@ public class coinsMinus extends Position {
     }
     
     public triggerEvent(Character Boy_X) {
-        System.out.println(Boy_X.getName() + " lost 5 coins!"
+        System.out.println(Boy_X.getName() + " lost 5 coins!");
         Boy_X.addCoins(-4);
     }
 }
