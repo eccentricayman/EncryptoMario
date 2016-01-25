aa9e9b7cf361cd03c6404a096e8b3f94b74bd0d9
Kevin <jh2933@gmail.com>
2016-01-25 02:11:21 +0000

    StatCheck changes

--- StatCheck.java
+++ StatCheck.java
@@ -1,55 +1,17 @@
-/* Statcheck is a java class that only let's a player pass if their 
-stats are at the required level, it will be implemented into position. */
-
-import java.util.ArrayList;
-
-public class StatCheck {
+public class StatChange extends Position {
     
-    private int strnreq;
-    private int intlreq;
-    private int dextreq;
-    private ArrayList<Character> players;
-    private Object special;
-
-    public StatCheck(int strn, int intl, int dext) {
-        strnreq = strn;
-        intlreq= intl;
-        dextreq = dext;
-    }
-
-    public void addplayer(Character toadd) {
-        if (players.indexOf(toadd) == -1) {
-            players.add(toadd);
-        }
-        else {
-            System.out.println("This player is already at this position.");
-        }
+    public StatChange() {
+        super("StatsChange");
     }
     
-    public void removeplayer(Character torm) {
-        if (players.indexOf(torm) != -1) {
-            players.remove(torm);
+    public triggerEvent(Character Boy_X) {
+        int chance = ((int)(Math.random()*8 )) + 1;
+        int val = ((int)(Math.random()*10)) + 1;
+        if (chance == 1) {
+            System.out.println("System Voice: " + Boy_X.getName() + " has gained " + val + " in Strength." 
         }
-        else {
-            System.out.println("This player is not at this position.");
-        }
-    }
-    
-    public boolean statCheck(Character tochk) {
-        if (players.indexOf(tochk) != -1) {
-            if (tochk.getStrength() >= strnreq &&
-                tochk.getIntelligence() >= intlreq &&
-                tochk.getDexterity() >= dextreq) {
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-        else {
-            System.out.println("Player is not at this position, so false.");
-            return false;
+        else if (chance == 2) {
+            System.out.println("System Voice: " + Boy_X.getName() + " has gained " + val + " in Strength." 
         }
     }
-    
 }
